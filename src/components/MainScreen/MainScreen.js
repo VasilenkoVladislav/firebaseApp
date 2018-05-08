f43d4f4df8168b65fd6c7269a21e6cee4968c9ec
VladislavVasilenko <vladislavva@123software.ru>
2018-05-08 16:25:22 +0300

    Todo link maps

--- src/components/MainScreen/MainScreen.js
+++ src/components/MainScreen/MainScreen.js
@@ -1,4 +1,5 @@
 import { View, Text, TouchableOpacity } from 'react-native';
+import MapView from 'react-native-maps';
 import React from 'react';
 import { styles } from './styles';
 import PropTypes from 'prop-types';
@@ -10,11 +11,21 @@ const propTypes = {
 const MainScreen = ({ signOut }) => {
     return (
         <View style={styles.container}>
+            <MapView
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            />
             <TouchableOpacity onPress={signOut} style={styles.logoutButton}>
                 <Text>LOGOUT</Text>
             </TouchableOpacity>
         </View>
     )
 };
+
+MainScreen.propTypes = propTypes;
 
 export default MainScreen;
